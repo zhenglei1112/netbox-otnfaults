d342ee1f64ffffa028fa98215c0343ee973d1083
zhenglei <zl@zhhao.net>
2025-11-25 16:28:08 +0800

    新增故障影响业务时，设置时间默认值，可连续添加影响的业务

--- netbox_otnfaults/forms.py
+++ netbox_otnfaults/forms.py
@@ -44,3 +44,22 @@ class OtnFaultImpactForm(NetBoxModelForm):
             'service_interruption_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
             'service_recovery_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # 如果从URL参数传递了故障ID，设置默认值
+        if 'initial' in kwargs and 'otn_fault' in kwargs['initial']:
+            try:
+                fault_id = kwargs['initial']['otn_fault']
+                fault = OtnFault.objects.get(pk=fault_id)
+                
+                # 设置默认时间值，确保格式正确
+                if fault.fault_occurrence_time:
+                    # 转换为datetime-local输入格式
+                    self.fields['service_interruption_time'].initial = fault.fault_occurrence_time.strftime('%Y-%m-%dT%H:%M')
+                if fault.fault_recovery_time:
+                    self.fields['service_recovery_time'].initial = fault.fault_recovery_time.strftime('%Y-%m-%dT%H:%M')
+                    
+            except (OtnFault.DoesNotExist, ValueError):
+                pass
--- netbox_otnfaults/views.py
+++ netbox_otnfaults/views.py
@@ -1,4 +1,5 @@
 from netbox.views import generic
+from django.shortcuts import render
 from .models import OtnFault, OtnFaultImpact
 from .forms import OtnFaultForm, OtnFaultImpactForm
 from .filtersets import OtnFaultFilterSet, OtnFaultImpactFilterSet
@@ -38,6 +39,62 @@ class OtnFaultImpactView(generic.ObjectView):
 class OtnFaultImpactEditView(generic.ObjectEditView):
     queryset = OtnFaultImpact.objects.all()
     form = OtnFaultImpactForm
+
+    def get(self, request, *args, **kwargs):
+        """
+        GET request handler
+            Overrides the ObjectEditView function to include form initialization
+            with data from the parent fault object
+
+        Args:
+            request: The current request
+        """
+        obj = self.get_object(**kwargs)
+        obj = self.alter_object(obj, request, args, kwargs)
+        model = self.queryset.model
+
+        # 从URL参数获取初始数据
+        initial_data = {}
+        otn_fault_id = request.GET.get('otn_fault')
+        if otn_fault_id:
+            try:
+                fault = OtnFault.objects.get(pk=otn_fault_id)
+                initial_data['otn_fault'] = otn_fault_id
+                
+                # 设置时间字段的默认值
+                if fault.fault_occurrence_time:
+                    initial_data['service_interruption_time'] = fault.fault_occurrence_time
+                if fault.fault_recovery_time:
+                    initial_data['service_recovery_time'] = fault.fault_recovery_time
+                    
+            except (OtnFault.DoesNotExist, ValueError):
+                pass
+
+        form = self.form(instance=obj, initial=initial_data)
+
+        return render(
+            request,
+            self.template_name,
+            {
+                'model': model,
+                'object': obj,
+                'form': form,
+                'return_url': self.get_return_url(request, obj),
+                **self.get_extra_context(request, obj),
+            },
+        )
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        
+        if '_addanother' in request.POST and response.status_code == 302:
+            otn_fault_id = request.POST.get('otn_fault')
+            if otn_fault_id:
+                location = response['Location']
+                delimiter = '&' if '?' in location else '?'
+                response['Location'] = f"{location}{delimiter}otn_fault={otn_fault_id}"
+                
+        return response
 
 class OtnFaultImpactDeleteView(generic.ObjectDeleteView):
     queryset = OtnFaultImpact.objects.all()
