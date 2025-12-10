60e7c41d89fd1c6b8b883da3c8b048fe6cfb3edd
zhenglei <zl@zhhao.net>
2025-12-10 10:49:10 +0800

    故障地图的Globe模式

--- netbox_otnfaults/urls.py
+++ netbox_otnfaults/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('faults/edit/', views.OtnFaultBulkEditView.as_view(), name='otnfault_bulk_edit'),
     path('faults/bulk-delete/', views.OtnFaultBulkDeleteView.as_view(), name='otnfault_bulk_delete'),
     path('faults/map/', views.OtnFaultMapView.as_view(), name='otnfault_map'),
+    path('faults/map-globe/', views.OtnFaultGlobeMapView.as_view(), name='otnfault_map_globe'),
     path('faults/<int:pk>/', include(get_model_urls('netbox_otnfaults', 'otnfault'))),
     path('faults/<int:pk>/edit/', views.OtnFaultEditView.as_view(), name='otnfault_edit'),
     path('faults/<int:pk>/delete/', views.OtnFaultDeleteView.as_view(), name='otnfault_delete'),
