b89b53a16fb4f87a73460fe3b2a6c530ce6f51e2
zhenglei <zl@zhhao.net>
2026-02-26 14:47:13 +0800

    修改菜单

--- netbox_otnfaults/navigation.py
+++ netbox_otnfaults/navigation.py
@@ -1,57 +1,66 @@
-from netbox.plugins import PluginMenuItem, PluginMenuButton
+from netbox.plugins import PluginMenu, PluginMenuItem, PluginMenuButton
 
-menu_items = (
-    PluginMenuItem(
-        link='plugins:netbox_otnfaults:otnfault_list',
-        link_text='故障登记',
-        buttons=(
-            PluginMenuButton(
-                link='plugins:netbox_otnfaults:otnfault_add',
-                title='添加',
-                icon_class='mdi mdi-plus-thick',
+menu = PluginMenu(
+    label='故障管理',
+    groups=(
+        ('故障', (
+            PluginMenuItem(
+                link='plugins:netbox_otnfaults:otnfault_list',
+                link_text='故障登记',
+                buttons=(
+                    PluginMenuButton(
+                        link='plugins:netbox_otnfaults:otnfault_add',
+                        title='添加',
+                        icon_class='mdi mdi-plus-thick',
+                    ),
+                )
             ),
-        )
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_otnfaults:otnfaultimpact_list',
-        link_text='故障影响业务',
-        buttons=(
-            PluginMenuButton(
-                link='plugins:netbox_otnfaults:otnfaultimpact_add',
-                title='添加',
-                icon_class='mdi mdi-plus-thick',
+            PluginMenuItem(
+                link='plugins:netbox_otnfaults:otnfaultimpact_list',
+                link_text='故障影响业务',
+                buttons=(
+                    PluginMenuButton(
+                        link='plugins:netbox_otnfaults:otnfaultimpact_add',
+                        title='添加',
+                        icon_class='mdi mdi-plus-thick',
+                    ),
+                )
             ),
-        )
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_otnfaults:otnpathgroup_list',
-        link_text='路径组',
-        buttons=(
-            PluginMenuButton(
-                link='plugins:netbox_otnfaults:otnpathgroup_add',
-                title='添加',
-                icon_class='mdi mdi-plus-thick',
+        )),
+        ('路径', (
+            PluginMenuItem(
+                link='plugins:netbox_otnfaults:otnpathgroup_list',
+                link_text='路径组',
+                buttons=(
+                    PluginMenuButton(
+                        link='plugins:netbox_otnfaults:otnpathgroup_add',
+                        title='添加',
+                        icon_class='mdi mdi-plus-thick',
+                    ),
+                )
             ),
-        )
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_otnfaults:otnpath_list',
-        link_text='路径管理',
-        buttons=(
-            PluginMenuButton(
-                link='plugins:netbox_otnfaults:otnpath_add',
-                title='添加',
-                icon_class='mdi mdi-plus-thick',
+            PluginMenuItem(
+                link='plugins:netbox_otnfaults:otnpath_list',
+                link_text='路径管理',
+                buttons=(
+                    PluginMenuButton(
+                        link='plugins:netbox_otnfaults:otnpath_add',
+                        title='添加',
+                        icon_class='mdi mdi-plus-thick',
+                    ),
+                )
             ),
-        )
+        )),
+        ('地图', (
+            PluginMenuItem(
+                link='plugins:netbox_otnfaults:otnfault_map_globe',
+                link_text='故障分布图',
+            ),
+            PluginMenuItem(
+                link='plugins:netbox_otnfaults:route_editor',
+                link_text='线路设计器',
+            ),
+        )),
     ),
-    PluginMenuItem(
-        link='plugins:netbox_otnfaults:otnfault_map_globe',
-        link_text='故障分布图',
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_otnfaults:route_editor',
-        link_text='线路设计器',
-    ),
+    icon_class='mdi mdi-alert-circle-outline',
 )
-
